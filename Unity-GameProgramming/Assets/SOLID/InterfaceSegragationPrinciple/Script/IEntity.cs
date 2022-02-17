98ba88f25fd0efd81070dd3139149b578141b095
Simla Beken <38881186+Simla4@users.noreply.github.com>
2022-02-17 18:45:03 +0300

    Added: SOLID --> Interface Segragetion Principle

---
+++ Unity-GameProgramming/Assets/SOLID/InterfaceSegragationPrinciple/Script/IEntity.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mesmerize
+{
+
+}
+
+public interface IEntity 
+{
+    /*
+     We have an iterface awith attack and health information in it.
+     We may not always need health and attack information at the same time.
+     If we write them all together, we will be calling information that we do not need. 
+     So we have to divide this information into different interfaces.
+     */
+
+    void MeMesmerize(IEntity ownerEntity);
+
+    int STR { get; }
+    int STA { get; }
+    int COS { get; }
+    int WIS { get; }
+    int INT { get; }
+    int CHA { get; }
+
+    float Health { get; set; }
+    int MaxHealth { get; }
+    int ModifyHealth(int amount);
+    
+}
+
+public class NPC : IAttack
+{
+}
+
+public class Door : IHealth
+{
+    
+    float Health { get; set; }
+    int MaxHealth { get; }
+    int ModifyHealth(int amount)
+    {
+       return (int)(Health -= amount);
+    }
+}
+
+
+public interface IHealth
+{
+    float Health { get; set; }
+    int MaxHealth { get; }
+    int ModifyHealth(int amount);
+}
+
+public interface IAttack
+{
+    int STR { get; }
+    int STA { get; }
+    int COS { get; }
+    int WIS { get; }
+    int INT { get; }
+    int CHA { get; }
+}
